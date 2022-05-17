1b41e60028e6e0d486e584e1fa145ec4c8d887c5
Omer David <omerdavid321@gmail.com>
2022-05-17 22:20:47 +0300

    interface

---
+++ B22 Ex04 OmerDavid 308483437 RazielAlron 316061415/Ex04.Menus.Interfaces/MenuItem.cs
@@ -0,0 +1,40 @@
+﻿namespace Ex04.Menus.Interfaces
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class MenuItem
+    {
+        public MenuItem(string i_Title)
+        {
+            Title = i_Title;
+        }
+
+        private readonly List<IMenuItemClickObserver> m_ClickedObserver;
+
+        public string Title { get; private set; }
+
+        public void AddClickObserver(IMenuItemClickObserver i_ClickObserver)
+        {
+            m_ClickedObserver.Add(i_ClickObserver);
+        }
+
+        public void RemoveClickObserver(IMenuItemClickObserver i_ClickObserver)
+        {
+            m_ClickedObserver.Remove(i_ClickObserver);
+        }
+
+        public void PerformClick()
+        {
+            OnClicked();
+        }
+
+        private void OnClicked()
+        {
+            foreach (IMenuItemClickObserver clickedObserver in m_ClickedObserver)
+            {
+                clickedObserver.ReportClick();
+            }
+        }
+    }
+}
